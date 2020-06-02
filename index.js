1c9c4cf07b5363dd154d3b95f9bd1819ef9565f7
Carlo Vallerga <carlo.vallerga@gmail.com>
2020-06-02 21:31:52 +0200

    Rename label-has-for with the new name label-has-associated-control

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
         'viralize-es6'
     ],
     rules: {
-        'jsx-a11y/label-has-for': 'off',
+        'jsx-a11y/label-has-associated-control': 'off',
         'jsx-a11y/no-static-element-interactions': 'off',
         'react/jsx-filename-extension': 'off',
         'react/destructuring-assignment': 'off',
